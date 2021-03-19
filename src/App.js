4cc51618f52fab8ac025fb9ba68ca3f00089e80b
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-03-19 14:35:28 +0530

    Enable chat engine and sign in

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 const App = () => {
     return(
         <ChatEngine
-            height="100vh"
-            projectId=""
-            userName=""
-            userSecret=""
+            height='100vh'
+            userName='nikila99'
+            userSecret='123123'
+            projectID='8e9d9e73-2a07-42e4-97e3-2c2488c0d903'
         />
     )
 }
